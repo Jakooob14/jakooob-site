f1bcbfa1fda1cb6c535e26ebddbc7b3d1ce4ae27
Jakooob14 <62592957+Jakooob14@users.noreply.github.com>
2022-02-24 19:11:53 +0100

    Update
    404 home added
    general moved ::selection and .btn

--- script-index.js
+++ script-index.js
@@ -33,25 +33,52 @@ function settingsToggle() {
     }
 }
 
-window.addEventListener("mousedown", (event) => {
-    const cursor = document.querySelector(".custom-cursor");
-    if (!cursor.classList.contains("click")) {
-        cursor.classList.add("click");
+function onload() {
+    bgColorChange(getCookie('bgColor')); 
+    itemColorChange(getCookie('itemColor')); 
+    accColorChange(getCookie('accColor'));
+    txtColorChange(getCookie('txtColor'));
+    borderColorChange(getCookie('borderColor'));
+}
 
-        setTimeout(() => {
-            cursor.classList.remove("click");
-        }, 400);
-    }
-});
+function bgColorChange(color){
+    document.cookie = 'bgColor=' + color + "; max-age=9999999999; secure";
+    document.documentElement.style.setProperty('--color-bg', color);
+}
 
-window.addEventListener("mousemove", (event) => {
-    const cursor = document.querySelector(".custom-cursor");
-    let x = event.pageX - cursor.offsetWidth / 2,
-        y = event.pageY - cursor.offsetHeight / 2;
-        cursorSize = getComputedStyle(document.documentElement).getPropertyValue('--cursor-size').replace('px','');
-        width = window.innerWidth - cursorSize - 20;
-        if(x<width){
-            cursor.style.left = `${x}px`;
-            cursor.style.top = `${y}px`;
+function txtColorChange(color) {
+    document.cookie = 'txtColor=' + color + "; max-age=9999999999; secure";
+    document.documentElement.style.setProperty('--color-text', color);
+}
+
+function accColorChange(color) {
+    document.cookie = 'accColor=' + color + "; max-age=9999999999; secure";
+    document.documentElement.style.setProperty('--accent-color-1', color);
+}
+
+function itemColorChange(color) {
+    document.cookie = 'itemColor=' + color + "; max-age=9999999999; secure";
+    document.documentElement.style.setProperty('--color-itemsbg', color);
+}
+
+function borderColorChange(color) {
+    document.cookie = 'borderColor=' + color + "; max-age=9999999999; secure";
+    document.documentElement.style.setProperty('--color-border', color);
+}
+
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
         }
-});
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
