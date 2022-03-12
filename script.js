4828083786a218ba60a1b9d34544659850303154
Jakooob14 <62592957+Jakooob14@users.noreply.github.com>
2022-03-12 20:04:28 +0100

    new projects page and mobile header

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMenu(){
+    var menuContainer = document.getElementById('menu-container');
+    var header = document.querySelector('header');
+    var menuLine1 = document.getElementById('menuLine1');
+    var menuLine2 = document.getElementById('menuLine2');
+    if(!menuContainer.classList.contains('menuOpen')){
+        menuContainer.classList.add('menuOpen');
+        menuLine1.classList.add('menuOpen');
+        menuLine2.classList.add('menuOpen');
+        menuContainer.style.top = 'calc(4rem + 6px)';
+        header.style.position = 'fixed';
+    } else {
+        menuContainer.classList.remove('menuOpen');
+        menuLine1.classList.remove('menuOpen');
+        menuLine2.classList.remove('menuOpen');
+        menuContainer.style.top = '-100vh';
+        header.style.position = 'absolute';
+    }
+}
