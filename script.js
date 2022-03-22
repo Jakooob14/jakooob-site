02f19964493c2a56cc7143e3a43609c64aa2e4f0
Jakooob14 <62592957+Jakooob14@users.noreply.github.com>
2022-03-22 21:44:23 +0100

    added scroll snap to main and projects page

--- script.js
+++ script.js
@@ -8,12 +8,10 @@ function toggleMenu(){
         menuLine1.classList.add('menuOpen');
         menuLine2.classList.add('menuOpen');
         menuContainer.style.top = 'calc(4rem + 6px)';
-        header.style.position = 'fixed';
     } else {
         menuContainer.classList.remove('menuOpen');
         menuLine1.classList.remove('menuOpen');
         menuLine2.classList.remove('menuOpen');
         menuContainer.style.top = '-100vh';
-        header.style.position = 'absolute';
     }
 }
