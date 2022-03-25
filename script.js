ea9af8c937855127523d0753689b5e34230de2c2
Jakooob14 <62592957+Jakooob14@users.noreply.github.com>
2022-03-25 16:49:09 +0100

    animation

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+function setCookie(cname, cvalue, exdays) {
+    if(exdays == 'session'){
+        document.cookie = cname + "=" + cvalue + ";path=/";
+    } else{
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+/////////////// ^ Cookies ^ ///////////////
+
 function toggleMenu(){
     var menuContainer = document.getElementById('menu-container');
     var header = document.querySelector('header');
@@ -13,5 +42,20 @@ function toggleMenu(){
         menuLine1.classList.remove('menuOpen');
         menuLine2.classList.remove('menuOpen');
         menuContainer.style.top = '-100vh';
+    }
+}
+
+function themeChange(){  
+    var theme = getCookie('theme');
+    var root = document.documentElement.style;
+    if(theme == 'light'){
+        setCookie('theme', 'dark', 'session');
+        root.setProperty('--color-bg', '#fff');
+    } else if(theme == 'dark'){
+        setCookie('theme', 'light', 'session');
+        root.setProperty('--color-bg', '#333');
+    } else{
+        setCookie('theme', 'light', 'session');
+        themeChange();
     }
 }
